805cef7153b2321c3209147dd4e088870ebf972e
Renan <renan.biegel@gmail.com>
2017-08-29 15:46:31 -0300

    Fixed templates and base location. Added

--- familyrecipes/settings/local.py
+++ familyrecipes/settings/local.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'familyrecipes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../../templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'familyrecipes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
